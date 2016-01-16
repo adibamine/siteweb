1b281df98d2a0068dc0a5de14d479d883a55cf53
adibamine <adib.amine.mdr@gmail.com>
2016-01-16 23:49:27 +0000

    Adding home/about pages with footer + routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+   def index
+   	render :file => 'pages/home.html.erb' and return
+   end
+
+   def about
+	render :file => 'pages/about.html.erb' and return
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'application#index'
+  get 'about' => 'application#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
